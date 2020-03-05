e9a112df869012670ef7e4add82de31f55902d9f
Dominik Jastrzębski <dominik@98elements.com>
2020-03-05 10:48:09 +0100

    Add a SOAP server

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# soap-client-security server
+
+This is a simple SOAP server featuring 2-way SSL and WS-Security.
+
+It's based on [gs-producing-web-service](https://github.com/spring-guides/gs-producing-web-service) from Spring Guides.
+
+To start the server, run:
+
+```bash
+./gradlew bootRun
+```
---
+++ server/src/main/java/com/_98elements/soapclientsecurity/server/WebServiceConfig.java
@@ -0,0 +1,42 @@
+package com._98elements.soapclientsecurity.server;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+    }
+}
